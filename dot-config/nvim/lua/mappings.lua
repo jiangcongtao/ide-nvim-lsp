8b69fff08ef56c41bcacbf46c2eb9b7a3df7412b
Nick Jiang <congtao.jiang@outlook.com>
2021-12-28 16:53:10 +0800

    Re-organize mappings

--- dot-config/nvim/lua/mappings.lua
+++ dot-config/nvim/lua/mappings.lua
@@ -108,18 +108,3 @@ map("n", "<S-Tab>", ":bprevious<CR>")
 -- -- Symbols outline
 -- map("n", "<leader>o", ":SymbolsOutline<cr>")
 
--- lsp
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-map('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-map('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
--- dot-config/nvim/lua/setup/lsp-installer.lua
+++ dot-config/nvim/lua/setup/lsp-installer.lua
@@ -1,13 +1,51 @@
 local lsp_installer = require("nvim-lsp-installer")
+
+local buf_map = function(bufnr, mode, lhs, rhs, opts)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or {
+        silent = true
+    })
+end
+
 local on_attach = function(client, bufnr)
-    print('*** lsp-installer.lua: on_attach called! *** ' .. bufnr)
     -- print(vim.inspect(client))
+    vim.cmd("command! LlspDef lua vim.lsp.buf.definition()")
+    vim.cmd("command! LlspFormatting lua vim.lsp.buf.formatting()")
+    vim.cmd("command! LlspCodeAction lua vim.lsp.buf.code_action()")
+    vim.cmd("command! LlspHover lua vim.lsp.buf.hover()")
+    vim.cmd("command! LlspRename lua vim.lsp.buf.rename()")
+    vim.cmd("command! LlspRefs lua vim.lsp.buf.references()")
+    vim.cmd("command! LlspTypeDef lua vim.lsp.buf.type_definition()")
+    vim.cmd("command! LlspImplementation lua vim.lsp.buf.implementation()")
+    vim.cmd("command! LlspDiagPrev lua vim.diagnostic.goto_prev()")
+    vim.cmd("command! LlspDiagNext lua vim.diagnostic.goto_next()")
+    vim.cmd("command! LlspDiagLine lua vim.diagnostic.open_float()")
+    vim.cmd("command! LlspSignatureHelp lua vim.lsp.buf.signature_help()")
+
+    -- lsp
+    buf_map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+    buf_map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    buf_map(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    buf_map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    buf_map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+    buf_map(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    buf_map(bufnr, 'n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+    buf_map(bufnr, 'n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+    buf_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+    buf_map(bufnr, 'n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+    buf_map(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+    buf_map(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+    buf_map(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+    buf_map(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+
+    print('* lsp-installer.lua: on_attach called! ***')
 end
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-    local opts = {on_attach = on_attach}
+    local opts = {
+        on_attach = on_attach
+    }
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
     --     opts.root_dir = function() ... end
